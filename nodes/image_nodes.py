3cb4388689b1e046397b632a28972945ca3e40df
norgeous <norgeous@users.noreply.github.com>
2025-08-09 12:40:37 +0100

    Update image_nodes.py

--- nodes/image_nodes.py
+++ nodes/image_nodes.py
@@ -6,11 +6,10 @@ def pil2tensor(image):
     return torch.from_numpy(np.array(image).astype(np.float32) / 255.0).unsqueeze(0)
 
 class Chunker:
-    RETURN_TYPES = ("IMAGE", "IMAGE", "IMAGE", "MASK", "INT",)
-    RETURN_NAMES = ("before_images","after_images", "control_video", "control_masks", "length",)
+    RETURN_TYPES = ("CHUNK_INFO", "IMAGE", "MASK", "INT",)
+    RETURN_NAMES = ("chunk_info", "control_video", "control_masks", "length",)
     OUTPUT_TOOLTIPS = (
-        "Images before this chunk, excluding overlap frames",
-        "Images after this chunk",
+        "chunk_info",
         "Chunk of control_video for WanVaceToVideo",
         "Chunk of control_masks for WanVaceToVideo",
         "The length of this chunk",
@@ -38,7 +37,7 @@ class Chunker:
     def chunkfrombatch(self, mode, index, length, overlap, images=None, control_video=None, control_masks=None):
         extend_from_images = mode == "extend"
 
-        images_before = None
+        images_before = None #torch.empty(0, 1)
         images_overlap = None
         # images_chunk = None
         images_after = None
@@ -47,8 +46,8 @@ class Chunker:
         adjusted_overlap = 0 if index == 0 else overlap # exclude overlap in first chunk
         adjusted_length = length - adjusted_overlap
 
-        w = images.shape[2]
-        h = images.shape[1]
+        w = images.shape[2] if images is not None else 500
+        h = images.shape[1] if images is not None else 500
 
         overlap_start = index * adjusted_length
         chunk_start = overlap_start + adjusted_overlap
@@ -60,6 +59,12 @@ class Chunker:
         if control_video is not None and len(control_video) < after_start:
             control_video = torch.cat((control_video, control_video[-1].repeat(after_start - len(control_video), 1, 1, 1)), dim=0)
 
+
+
+
+
+
+
         # select chunk of images if provided
         if images is not None:
             image_count = len(images)
@@ -68,29 +73,45 @@ class Chunker:
             # images_chunk = images[chunk_start:after_start]
             images_after = images[after_start:image_count]
 
+
+
+
+
+
         grey_panel  = pil2tensor(Image.new('RGB', (w, h), (128, 128, 128)))
 
         # create control_video
         control_video_chunk = []
-        control_video_chunk.extend([images_overlap])
+        if images is not None:
+            control_video_chunk.extend([images_overlap])
         if control_video is None:
-            if extend_from_images and index == 0:
+            if images is not None and extend_from_images and index == 0:
+                #print("start with real image")
                 control_video_chunk.append(images[0:1])
                 control_video_chunk.extend([grey_panel] * (adjusted_length - 1))
             else:
+                #print('all grey images')
                 control_video_chunk.extend([grey_panel] * adjusted_length)
         else:
             control_video_chunk.extend([control_video[chunk_start:after_start]])
+        #print('images',images)
+        #print('cvlen',len(control_video_chunk))
+        #print('cv',control_video_chunk)
         control_video_torch = torch.cat(control_video_chunk, dim=0)
+
+
+
+
 
         black_panel = pil2tensor(Image.new('RGB', (w, h), (0,   0,   0  )).convert('L'))
         white_panel = pil2tensor(Image.new('RGB', (w, h), (255, 255, 255)).convert('L'))
 
         # create control_masks
         control_masks_chunk = []
-        control_masks_chunk.extend([black_panel] * (1 if extend_from_images and index == 0 else adjusted_overlap))
+        if images is not None:
+            control_masks_chunk.extend([black_panel] * (1 if extend_from_images and index == 0 else adjusted_overlap))
         if control_masks is None:
-            if extend_from_images and index == 0:
+            if images is not None and extend_from_images and index == 0:
                 control_masks_chunk.extend([white_panel] * (adjusted_length - 1))
             else:
                 control_masks_chunk.extend([white_panel] * adjusted_length)
@@ -98,10 +119,68 @@ class Chunker:
             control_masks_chunk.extend([control_masks[overlap_start:after_start]])
         control_masks_torch = torch.cat(control_masks_chunk, dim=0)
 
-        return (
+        chunk_info = (
             images_before,
             images_after,
+        )
+
+        #print("chunk_info send", chunk_info)
+
+        return (
+            chunk_info,
             control_video_torch,
             control_masks_torch,
             length,
         )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class ImageBatchMulti:
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ("images",)
+    FUNCTION = "combine"
+    CATEGORY = "Chunker"
+    DESCRIPTION = "Chunker Combine"
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "chunk_info": ("CHUNK_INFO", {"forceInput": True}),
+                "images": ("IMAGE", ),
+            },
+    }
+
+    def combine(self, chunk_info, images):
+        output = []
+        print("chunk_info get", chunk_info)
+        if chunk_info[0] is not None:
+            output.extend([chunk_info[0]])
+        if images is not None:
+            output.extend([images])
+        if chunk_info[1] is not None:
+            output.extend([chunk_info[1]])
+        output_torch = torch.cat(output, dim=0)
+        return (output_torch,)
+

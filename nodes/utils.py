c65ff003730b508a2f4fb57e6f426722dcb78cda
norgeous <norgeous@users.noreply.github.com>
2025-08-15 15:05:47 +0100

    Update utils.py

--- nodes/utils.py
+++ nodes/utils.py
@@ -24,7 +24,7 @@ def len2(thing):
         count += len(item)
     return count
 
-def kijaiWanResize(image, mask, generation_width, generation_height, aspect_ratio):
+def kijaiWanResizeCalc(image, generation_width, generation_height, aspect_ratio):
     VAE_STRIDE = (4, 8, 8)
     PATCH_SIZE = (1, 2, 2)
     H, W = image.shape[1], image.shape[2]
@@ -44,6 +44,16 @@ def kijaiWanResize(image, mask, generation_width, generation_height, aspect_rati
         PATCH_SIZE[2] * PATCH_SIZE[2])
     h = lat_h * VAE_STRIDE[1]
     w = lat_w * VAE_STRIDE[2]
-    resized_image = common_upscale(image.movedim(-1, 1), w, h, "lanczos", crop).movedim(1, -1) if image is not None else None
-    resized_mask = common_upscale(mask.unsqueeze(1).repeat(1, 3, 1, 1), w, h, "lanczos", crop).movedim(1,-1)[:, :, :, 0] if mask is not None else None
-    return (resized_image, resized_mask)
+    return (w, h, crop)
+
+def resizeImage(image, width, height, aspect_ratio):
+    if image is None: return None
+    w, h, crop = kijaiWanResizeCalc(image, width, height, aspect_ratio)
+    resized_image = common_upscale(image.movedim(-1, 1), w, h, "lanczos", crop).movedim(1, -1)
+    return resized_image
+
+def resizeMask(mask, width, height, aspect_ratio):
+    if mask is None: return None
+    w, h, crop = kijaiWanResizeCalc(mask, width, height, aspect_ratio)
+    resized_mask = common_upscale(mask.unsqueeze(1).repeat(1, 3, 1, 1), w, h, "lanczos", crop).movedim(1,-1)[:, :, :, 0]
+    return resized_mask

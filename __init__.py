227e970dfc2830a883e4af46d08c09a8bae8648c
norgeous <norgeous@users.noreply.github.com>
2025-09-12 09:16:54 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,22 +2,16 @@
 from .nodes.nodes import *
 
 NODE_CONFIG = {
-    "ChunkerConfig": {"class": ChunkerConfig, "name": "\U0001F36B Config"},
-    "ChunkerResequencer": {"class": ChunkerResequencer, "name": "\U0001F36B Resequencer"},
     "Chunker": {"class": Chunker, "name": "\U0001F36B Chunker"},
     "ChunkerCombine": {"class": ChunkerCombine, "name": "\U0001F36B Combine"},
 }
 
 NODE_CLASS_MAPPINGS = {
-    "ChunkerConfig": ChunkerConfig,
-    "ChunkerResequencer": ChunkerResequencer,
     "Chunker": Chunker,
     "ChunkerCombine": ChunkerCombine,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "ChunkerConfig": "\U0001F36B Config",
-    "ChunkerResequencer": "\U0001F36B Resequencer",
     "Chunker": "\U0001F36B Chunker",
     "ChunkerCombine": "\U0001F36B Combine",
 }

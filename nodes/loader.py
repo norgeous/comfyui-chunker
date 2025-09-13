d4b00e5cc86652746c30eb7a3c6c749a79e973c2
norgeous <norgeous@users.noreply.github.com>
2025-09-13 18:52:57 +0100

    Update loader.py

--- nodes/loader.py
+++ nodes/loader.py
@@ -3,15 +3,72 @@
 import torch
 import numpy as np
 import av
+from PIL import Image, ImageOps, ImageSequence
+import folder_paths
 
-def load_video_chunk(
-        video_path="/home/user/ComfyUI/input/#cosplay.mp4",
-        start_n=0,
-        end_n=None,
-    ):
+
+def pillow(fn, arg):
+    prev_value = None
+    try:
+        x = fn(arg)
+    except (OSError, UnidentifiedImageError, ValueError): #PIL issues #4472 and #2445, also fixes ComfyUI issue #3416
+        prev_value = ImageFile.LOAD_TRUNCATED_IMAGES
+        ImageFile.LOAD_TRUNCATED_IMAGES = True
+        x = fn(arg)
+    finally:
+        if prev_value is not None:
+            ImageFile.LOAD_TRUNCATED_IMAGES = prev_value
+    return x
+
+# https://github.com/IuvenisSapiens/ComfyUI_Qwen2_5-VL-Instruct/blob/main/util_nodes.py
+def load_image_advanced(image_path):
+    # image_path = folder_paths.get_annotated_filepath(image)
+
+    img = pillow(Image.open, image_path)
+
+    output_images = []
+    output_masks = []
+    w, h = None, None
+
+    excluded_formats = ["MPO"]
+
+    for i in ImageSequence.Iterator(img):
+        i = pillow(ImageOps.exif_transpose, i)
+
+        if i.mode == "I":
+            i = i.point(lambda i: i * (1 / 255))
+        image = i.convert("RGB")
+
+        if len(output_images) == 0:
+            w = image.size[0]
+            h = image.size[1]
+
+        if image.size[0] != w or image.size[1] != h:
+            continue
+
+        image = np.array(image).astype(np.float32) / 255.0
+        image = torch.from_numpy(image)[None,]
+        if "A" in i.getbands():
+            mask = np.array(i.getchannel("A")).astype(np.float32) / 255.0
+            mask = 1.0 - torch.from_numpy(mask)
+        else:
+            mask = torch.zeros((64, 64), dtype=torch.float32, device="cpu")
+        output_images.append(image)
+        output_masks.append(mask.unsqueeze(0))
+
+    if len(output_images) > 1 and img.format not in excluded_formats:
+        output_image = torch.cat(output_images, dim=0)
+        output_mask = torch.cat(output_masks, dim=0)
+    else:
+        output_image = output_images[0]
+        output_mask = output_masks[0]
+
+    return (output_image, output_mask)
+
+def load_video_chunk(video_path, start_n=0, end_n=None):
     container = av.open(video_path)
     stream = container.streams.video[0]
-    duration = container.duration / av.time_base
+    # duration = container.duration / av.time_base
     fps = stream.average_rate
     total_length = stream.frames
 
@@ -54,16 +111,17 @@ def load_video_chunk(
     print(f"itterated {len(all_frames_n)} frames and collected {len(frames)} frames")
     return (frames, fps, total_length)
 
-
 def awesome_loader(path, start, end):
     img_ext = ["jpeg", "jpg", "png"]
     vid_ext = ["mp4"]
     file_ext = path.replace(" [input]", "").split(".")[-1]
     if file_ext in img_ext:
-        image = None
+        # image_path = folder_paths.get_annotated_filepath(image)
+        image, mask = load_image_advanced(path)
         fps = 0
         total_length = 1
-        return (frames, fps, total_length)
+        return (image, mask, fps, total_length)
     if file_ext in vid_ext:
         frames, fps, total_length = load_video_chunk(path, start_n=start, end_n=end)
-        return (frames, fps, total_length)
+        mask = None
+        return (frames, mask, fps, total_length)

f87267a8983203f5bfb8751084626cbede80245b
Daniel Harte <norgeous@users.noreply.github.com>
2026-05-05 14:03:23 +0100

    Update utils_performance.py

--- lib/utils_performance.py
+++ lib/utils_performance.py
@@ -1,9 +1,4 @@
 import time
-import math
 
 def get_ts():
     return int(time.time() * 1000) # current time in milliseconds
-
-def predict(data, next_count):
-    avg = sum(data) / len(data)
-    return [avg] * next_count
